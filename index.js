2e60be45129df3d0aab442c723d6ad3cca95eeb9
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-04-23 16:24:15 +0000

    updated files

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function Area(){
 var diameter = document.getElementById("diameter");
 var radius = document.getElementById("radius");
 var diametervalue = +diameter.value;
-var radiusvalue = +(diameter/2).value;
-var result1 = ((diameter/2).value*(diameter/2).value*Math.PI);
-var result2  = (2*(diameter/2).value*Math.PI);
+var radiusvalue = +(diameter.value/2);
+var result1 = ((diameter.value/2)*(diameter.value/2)*Math.PI);
+var result2  = (diameter.value*Math.PI);
 var divresult1 = document.getElementById("divresult1");
 var divresult2 = document.getElementById("divresult2");
 divresult1.innerHTML = result1;
